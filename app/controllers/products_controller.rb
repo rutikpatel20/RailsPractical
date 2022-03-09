b858bf9f951596cc41f2d4cd936e5e1a7b2d414f
rutiksimform <rutik.p@simformsolutions.com>
2022-03-09 11:31:31 +0530

    style: TE5-T86 add proper indentation for readability and also add comments for better understanding of code

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,18 +1,23 @@
 class ProductsController < ApplicationController
+
+  # Index Method
   def index
     @products = Product.all
   end
 
+  # Method For Showing Products
   def show
     @product = Product.find(params[:id])
   end
 
+   # Method For Adding New Products
   def new
     @product = Product.new
   end
 
+   # Method For Creating Products
   def create
-    @product= Product.create(params.require(:product).permit(:name, :price, :description))
+    @product = Product.create(params.require(:product).permit(:name, :price, :description))
     if @product.valid?
       flash[:notice] = "Product Added!"
       redirect_to products_path
@@ -22,22 +27,24 @@ class ProductsController < ApplicationController
     end
   end
 
+   # Method For Edit Details of Products
   def edit
     @product = Product.find(params[:id])
   end
 
+   # Method For Updating Products
   def update
     @product = Product.find(params[:id])
     if @product.update(params.require(:product).permit(:name, :price, :brand))
-      flash[:notice]="Updated!"
-      
+      flash[:notice]="Product Updated!"
       redirect_to product_path(@product)
     else
-      flash[:errors]=@product.errors.full_messages
+      flash[:errors] = @product.errors.full_messages
       redirect_to edit_product_path(@product)
     end
   end
 
+   # Method For Destroying Product
   def destroy
     @product = Product.find(params[:id])   
     if @product.delete   
@@ -46,10 +53,7 @@ class ProductsController < ApplicationController
     else   
       flash[:errors] = @product.errors.full_messages
       redirect_to destroy_product_path
-  end
-end
-
-# def product_params   
-#   params.require(:product).permit(:name, :price, :description)
-# end   
+    end
+  end 
+  
 end
--- app/models/product.rb
+++ app/models/product.rb
@@ -1,5 +1,8 @@
 class Product < ApplicationRecord
+
+    # For Validation Purpose
     validates :name, presence: true   
-validates :price, presence: true, numericality: {:greater_than => 0}   
-validates :description, presence: true  
+    validates :price, presence: true, numericality: {:greater_than => 0}   
+    validates :description, presence: true  
+
 end
