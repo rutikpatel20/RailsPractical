b2306f4ba7782e8084609e151e148b4c024719c7
rutiksimform <rutik.p@simformsolutions.com>
2022-03-14 16:09:02 +0530

    style: TE5-T87 add proper indentations and comments

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,5 @@
 Rails.application.routes.draw do
-  get 'books/index'
-  get 'books/new'
-  get 'books/create'
-  get 'books/show'
-  get 'books/edit'
-  get 'books/update'
-  get 'books/destroy'
+ 
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
