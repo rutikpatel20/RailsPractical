0f0d7cbb5cdcf38f92fdc917c94dd604f2c85170
rutiksimform <rutik.p@simformsolutions.com>
2022-03-29 13:52:20 +0530

    feat: TE5-T91 add active record query interface practical

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,11 @@
 Rails.application.routes.draw do
+  get 'employees/index'
+  get 'employees/new'
+  get 'employees/create'
+  get 'employees/show'
+  get 'employees/edit'
+  get 'employees/update'
+  get 'employees/destroy'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
@@ -17,8 +24,15 @@ Rails.application.routes.draw do
 
   #for Faculties
   resources :faculties
-  root 'faculties#index'
+  # root 'faculties#index'
 
   #for Students
   resources :students
+
+  #for Employees
+  root "employees#index"
+  post '/increaseOrder', to: "employees#increaseOrder"
+  post '/decreaseOrder', to: "employees#decreaseOrder"
+  get 'employees/task'
+  resources :employees
 end
